09297c35f6bfdb7e76457d183bae9359f80aabff
graupnerj <101127619+graupnej@users.noreply.github.com>
2025-01-23 15:31:43 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,7 +30,7 @@ mutable struct WordEmbedding{S<:AbstractString, T<:Real}
 end
 
 @testset "get_vector_from_word" begin
-    words = ["cat", "dog", "bird", "fish"]
+    words = AbstractString["cat", "dog", "bird", "fish"]
     embeddings = [1.0 2.0 3.0 4.0; 5.0 6.0 7.0 8.0; 9.0 10.0 11.0 12.0]
     wv = WordEmbedding(words, embeddings)
     
