450f8e96db3fe7ba2fbe517d66890090ffd28b78
graupnerj <101127619+graupnej@users.noreply.github.com>
2025-01-24 12:25:31 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,13 @@
+using Documenter
+using GroupIWord2Vec  # Replace this with your package name
 
+makedocs(
+    modules = [GroupIWord2Vec],                   # Include the modules you want to document
+    authors="Julian Graupner <...>, Pablo Ramos Erpenbeck <...>, Knut Bunge <...>, Ladislaus Finger <...e>",
+    sitename = "GroupIWord2Vec.jl Documentation",  # Replace with your documentation title
+    format = Documenter.HTML(),                   # Generate HTML docs
+    pages = [
+        "Home" => "index.md",                     # Define the pages of the documentation
+    ],
+    clean = true,                                 # Clean previously built files
+)
