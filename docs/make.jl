bd175aee0309f1c940621262bff818dc7f6f3b1e
graupnerj <101127619+graupnej@users.noreply.github.com>
2025-01-29 16:22:46 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(
         "Getting Started" => "getting_started.md",
         "Functions" => "functions.md",
     ],
-    assets = String["../../assets/PCAProjection.png"]
+    assets = String["../assets/PCAProjection.png"]
 )
 
 deploydocs(;
