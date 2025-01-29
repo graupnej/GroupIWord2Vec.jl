f123c71dbf175f26218dace26c3337bb8bb7db59
05p4c3 <75911137+05p4c3@users.noreply.github.com>
2025-01-29 20:45:28 +0100

    Update GroupIWord2Vec.jl

--- src/GroupIWord2Vec.jl
+++ src/GroupIWord2Vec.jl
@@ -28,20 +28,18 @@ using Plots
 import Word2Vec_jll         # Links to the underlying Word2Vec implementation (C code)
 
 # Make these functions and types available to anyone using this module. Other programs can use these functions after importing GroupIWord2Vec
-export train_model,          # Function to train new word embeddings
-      WordEmbedding,         # The main data structure for word embeddings
-      load_embeddings,       # Function to load pre-trained embeddings
-      read_binary_format,    # Function to read binary embedding files
-      read_text_format,      # Function to read text embedding files
-      get_vector_from_word,            # Function to get a word's vector
-      get_word_from_vector,
-      cosine_similarity,     # Function to compute similarity of two words
-      get_top_similarity_of_word, # Function to find top n similar words as strings
-      get_top_similarity_of_vector,
-      word_analogy,
-      word_addition,
-      word_subtraction,
-      euclidean_distance,
-      show_relations
+export      train_model,            # Function to train new word embeddings
+            WordEmbedding,          # The main data structure for word embeddings
+            load_embeddings,        # Function to load pre-trained embeddings
+            read_binary_format,     # Function to read binary embedding files
+            read_text_format,       # Function to read text embedding files
+            get_word2vec,           # Function to get a word's embedded vector
+            get_vec2word,           # Function to get a vector's word
+            get_any2vec,            # Function to handle word/ vector input and convert input word/ vector into vector
+            get_cosine_similarity,  # Function to compute similarity of two words
+            get_similar_words,      # Function to find top n similar words as strings
+            get_vector_operation,   # Function to find perform operation on 2 input words/vectors: sum, subtract, dot-product, euclidean distance
+            get_word_analogy,       # Function to use vector math to compute analogous words
+            show_relations          # Function to visualise vectors and vector distances in 2D
 
 end
