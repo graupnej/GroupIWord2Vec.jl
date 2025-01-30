a8c1abd228ec9b5aed79b9076aa094e824ea8f79
graupnerj <101127619+graupnej@users.noreply.github.com>
2025-01-30 20:36:36 +0100

    Update show_relations.jl

--- src/show_relations.jl
+++ src/show_relations.jl
@@ -48,23 +48,47 @@ Note: Use an even number of inputs!
 function show_relations(words::String...; wv::WordEmbedding, save_path::String="word_relations.png")
     # Check input - word_count should only be used inside the function
     word_count = length(words)
-    if Bool(word_count%2)
-        throw(error("need words in multiples of 2 but $word_count are given"))
+    if word_count % 2 != 0
+        throw(ArgumentError("Need words in multiples of 2, but $word_count were given"))
     end
+
+    #if Bool(word_count%2)
+        #throw(error("need words in multiples of 2 but $word_count are given"))
+    #end
+
+    
+    # Create a dictionary mapping words to their indices (or `nothing` if missing)
+    #indices = Dict(word => get(wv.word_indices, word, nothing) for word in words)
+
+    # Find missing words
+    #missing_words = [word for (word, idx) in indices if idx === nothing]
+    
+    # If there are missing words, throw an error listing all of them
+    #if !isempty(missing_words)
+        #throw(ArgumentError("Words not found in embeddings: " * join(missing_words, ", ")))
+    #end
     
     # Check if all words exist in the embedding
+    # maybe improve with commented lines above
     for word in words
         if !haskey(wv.word_indices, word)
             throw(error("Word '$word' not found in embeddings"))
         end
     end
-    # Get embeddings by looking up each word's index and getting its vector                
+    # Get embeddings by looking up each word's index and getting its vector     
+    # maybe improve with: embeddings = permutedims(hcat([wv.embeddings[:, idx] for idx in values(indices)]...))
     embeddings = reduce(vcat, transpose.([wv.embeddings[:, wv.word_indices[word]] for word in words]))
 
+    # maybe improve with labels = text.(words, :bottom)
     labels = text.([word for word in words], :bottom)    
     
     # reduce dimension
     projection = reduce_to_2d(embeddings)
+
+    #maybe use this pair_count = div(word_count, 2)
+    # for this arrows = [projection[:, 2i] - projection[:, 2i-1] for i in 1:pair_count]
+    # arrows_x, arrows_y = first.(arrows), last.(arrows)
+
     
     # preparation for plotting the arrows, infill with zeros and split x, y
     arrows = [projection[:, 2*i]-projection[:, 2*i-1] for i in 1:Int(word_count/2)]
@@ -81,7 +105,9 @@ function show_relations(words::String...; wv::WordEmbedding, save_path::String="
     quiver!(p, projection[1, :], projection[2, :], quiver=(arrows_x, arrows_y))
     
     # Save the plot
-    savefig(p, save_path)
-    
+    if !isempty(save_path)
+        savefig(p, save_path)
+    end
+
     return p  # Optionally return the plot object
 end
