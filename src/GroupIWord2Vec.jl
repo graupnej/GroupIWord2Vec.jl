18a14a766133f884f284018982ed81710134add1
05p4c3 <75911137+05p4c3@users.noreply.github.com>
2025-01-30 21:51:00 +0100

    Update GroupIWord2Vec.jl

--- src/GroupIWord2Vec.jl
+++ src/GroupIWord2Vec.jl
@@ -12,7 +12,6 @@ This is the main module file that organizes all the word embedding functionality
 - `get_word2vec`:            Function to get a word's embedded vector
 - `get_vec2word`:            Function to get a vector's word
 - `get_any2vec`:             Function to handle word/ vector input and convert input word/ vector into vector
-- `get_cosine_similarity`:   Function to compute similarity of two words
 - `get_similar_words`:       Function to find top n similar words as strings
 - `get_vector_operation`:    Function to find perform operation on 2 input words/vectors: sum, subtract, dot-product, euclidean distance
 - `get_word_analogy`:        Function to use vector math to compute analogous words
