e459db0d045875c68cb40cfa079d538dbcf4a846
graupnerj <101127619+graupnej@users.noreply.github.com>
2025-01-31 19:26:44 +0100

    Update test_functions.jl

--- test/test_functions.jl
+++ test/test_functions.jl
@@ -5,7 +5,7 @@ using GroupIWord2Vec
     # Test data setup
     words = ["cat", "dog", "bird", "fish"]
     embeddings = Float64[1.0 2.0 3.0 4.0; 5.0 6.0 7.0 8.0; 9.0 10.0 11.0 12.0]
-
+    
     wv = WordEmbedding(words, embeddings)
 
     @testset "basic functionality" begin
